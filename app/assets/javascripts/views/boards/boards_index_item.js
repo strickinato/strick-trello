293f54001569697413b326c7bce58215cd1e751d
Aaron Strick <aaronstrick@gmail.com>
2014-11-21 13:29:53 -0800

    Added a bit of styling to the board-index-items

--- app/assets/javascripts/views/boards/boards_index_item.js
+++ app/assets/javascripts/views/boards/boards_index_item.js
@@ -2,6 +2,8 @@ TrelloClone.Views.BoardsIndexItem = Backbone.View.extend({
 
   template: JST["boards/index_item"],
 
+  className: "board-index-item",
+
   render: function() {
     var content = this.template({
       board: this.model.attributes
